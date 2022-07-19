0c1eb1e92d946fda996814e52d8a21b304d72e34
Kevin Rangel <code@venikx.com>
2022-07-19 22:02:34 +0300

    chore: initial python deps test

---
+++ default.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> { };
+
+let
+  pythonPackages = python3Packages;
+in pkgs.mkShell rec {
+  name = "impurePythonEnv";
+  venvDir = "./.venv";
+  packages = [
+    black
+  ];
+  buildInputs = [
+    # A Python interpreter including the 'venv' module is required to bootstrap
+    # the environment.
+    pythonPackages.python
+
+    # This executes some shell code to initialize a venv in $venvDir before
+    # dropping into the shell
+    pythonPackages.venvShellHook
+  ];
+
+  # Run this command, only after creating the virtual environment
+  postVenvCreation = ''
+    pip install -r requirements.txt
+  '';
+}
