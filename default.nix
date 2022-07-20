4067cea881003968cc6437b6c45a2cdf8aadc75b
Kevin Rangel <code@venikx.com>
2022-07-20 19:52:49 +0300

    chore: use poetry instead

--- default.nix
+++ default.nix
@@ -1,25 +1,7 @@
-with import <nixpkgs> { };
+{ pkgs ? import <nixpkgs> {} }:
 
-let
-  pythonPackages = python3Packages;
-in pkgs.mkShell rec {
-  name = "impurePythonEnv";
-  venvDir = "./.venv";
-  packages = [
-    black
-  ];
-  buildInputs = [
-    # A Python interpreter including the 'venv' module is required to bootstrap
-    # the environment.
-    pythonPackages.python
+with pkgs;
 
-    # This executes some shell code to initialize a venv in $venvDir before
-    # dropping into the shell
-    pythonPackages.venvShellHook
-  ];
-
-  # Run this command, only after creating the virtual environment
-  postVenvCreation = ''
-    pip install -r requirements.txt
-  '';
+poetry2nix.mkPoetryApplication {
+  projectDir = ./.;
 }
